adde62a05753987d14b9f8d57cf774fc28b233ca
Anthony Perritano <aperritano@gmail.com>
2012-02-06 13:24:30 +0100

    modules secure and crud are now local.
    They do not reference external play runtimes

--- app/bootstrap/Bootstrap.java
+++ app/bootstrap/Bootstrap.java
@@ -9,7 +9,7 @@ public class Bootstrap extends Job {
  
     public void doJob() {
         // Check if the database is empty
-            Fixtures.loadModels("initial-data.yml");
+            //Fixtures.loadModels("initial-data.yml");
     }
  
 }
