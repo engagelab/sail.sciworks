d1a9259e77194deda61680f6dde5ee85e5010058
Anthony Perritano <aperritano@gmail.com>
2012-02-07 09:52:31 +0100

    not need right now

--- app/bootstrap/Bootstrap.java
+++ app/bootstrap/Bootstrap.java
@@ -7,9 +7,9 @@ import play.test.Fixtures;
 @OnApplicationStart
 public class Bootstrap extends Job {
  
-    public void doJob() {
-        // Check if the database is empty
-            //Fixtures.loadModels("initial-data.yml");
-    }
+//    public void doJob() {
+//        // Check if the database is empty
+//        Fixtures.loadModels("initial-data.yml");
+//    }
  
 }
